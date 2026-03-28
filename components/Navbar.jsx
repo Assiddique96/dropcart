3cfe2656dfe20e121d964fb43ccc802ee2bd540a
Vincent <122548823+Vinnnnce@users.noreply.github.com>
2026-03-28 09:31:12 +0300

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,15 +27,7 @@ const Navbar = () => {
                 <div className="flex items-center justify-between max-w-7xl mx-auto py-4  transition-all">
 
                     <Link href="/" className="relative text-4xl font-semibold text-slate-400">
-                        <span className="text-gray-600">Drop</span>Cart<span className="text-gray-600 text-5xl leading-0">.</span>
-                        <p className="absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-gray-500">
-                            .NG
-                        </p>
-                        <Show when={{ plan: 'plus' }}>   
-                            <p className="absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-gray-500">
-                            NG
-                        </p>
-                        </Show>
+                        <span className="text-gray-600">Drop</span>Cart<span className="text-gray-600 text-5xl leading-0"></span>
                     </Link>
 
                     {/* Desktop Menu */}
