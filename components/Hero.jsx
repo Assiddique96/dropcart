e6ee963a9599a6aab50914874fedb9e04bd63c80
Vincent <122548823+Vinnnnce@users.noreply.github.com>
2026-03-28 07:36:22 +0300

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     return (
         <div className='mx-6'>
             <div className='flex max-xl:flex-col gap-8 max-w-7xl mx-auto my-10'>
-                <div className='relative flex-1 flex flex-col bg-green-200 rounded-3xl xl:min-h-100 group'>
+                <div className='relative flex-1 flex flex-col bg-gray-300 rounded-3xl xl:min-h-100 group'>
                     <div className='p-5 sm:p-16'>
                         <div className='inline-flex items-center gap-3 bg-green-300 text-green-600 pr-4 p-1 rounded-full text-xs sm:text-sm'>
                             <span className='bg-green-600 px-3 py-1 max-sm:ml-1 rounded-full text-white text-xs'>NEWS</span> Free Shipping on Orders Above {currency}1,000,000.00! <ChevronRightIcon className='group-hover:ml-2 transition-all' size={16} />
@@ -38,7 +38,7 @@ const Hero = () => {
                     </div>
                     <div className='flex-1 flex items-center justify-between w-full bg-blue-200 rounded-3xl p-6 px-8 group'>
                         <div>
-                            <p className='text-3xl font-medium bg-gradient-to-r from-slate-800 to-[#78B2FF] bg-clip-text text-transparent max-w-40'>20% discounts</p>
+                            <p className='text-3xl font-medium bg-gradient-to-r from-slate-800 to-[#263240] bg-clip-text text-transparent max-w-40'>20% discounts</p>
                             <p className='flex items-center gap-1 mt-4'>View more <ArrowRightIcon className='group-hover:ml-2 transition-all' size={18} /> </p>
                         </div>
                         <Image className='w-35' src={assets.hero_product_img2} alt="" />
