6396c4fb9fcbf136f16ec00e36fd89c433559d12
Vincent <122548823+Vinnnnce@users.noreply.github.com>
2026-03-28 09:25:25 +0300

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,10 +14,10 @@ const Hero = () => {
             <div className='flex max-xl:flex-col gap-8 max-w-7xl mx-auto my-10'>
                 <div className='relative flex-1 flex flex-col bg-gray-300 rounded-3xl xl:min-h-100 group'>
                     <div className='p-5 sm:p-16'>
-                        <div className='inline-flex items-center gap-3 bg-purple-200 text-green-600 pr-4 p-1 rounded-full text-xs sm:text-sm'>
-                            <span className='bg-green-600 px-3 py-1 max-sm:ml-1 rounded-full text-white text-xs'>NEWS</span> Free Shipping on Orders Above {currency}1,000,000.00! <ChevronRightIcon className='group-hover:ml-2 transition-all' size={16} />
+                        <div className='inline-flex items-center gap-3 bg-gray-900 text-gray-500 pr-4 p-1 rounded-full text-xs sm:text-sm'>
+                            <span className='bg-gray-400 px-3 py-1 max-sm:ml-1 rounded-full text-white text-xs'>NEWS</span> Free Shipping on Orders Above {currency}1,000,000.00! <ChevronRightIcon className='group-hover:ml-2 transition-all' size={16} />
                         </div>
-                        <h2 className='text-3xl sm:text-5xl leading-[1.2] my-3 font-medium bg-gradient-to-r from-slate-600 to-[#A0FF74] bg-clip-text text-transparent max-w-xs  sm:max-w-md'>
+                        <h2 className='text-3xl sm:text-5xl leading-[1.2] my-3 font-medium bg-gradient-to-r from-slate-600 to-[#595e57] bg-clip-text text-transparent max-w-xs  sm:max-w-md'>
                             Gadgets you'll love. Prices you'll trust.
                         </h2>
                         <div className='text-slate-800 text-sm font-medium mt-4 sm:mt-8'>
