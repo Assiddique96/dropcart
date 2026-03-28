deb4e08c14402e21345869e189e8e351a25cb870
Vincent <122548823+Vinnnnce@users.noreply.github.com>
2026-03-28 08:04:08 +0300

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
                     <Link href="/" className="relative text-4xl font-semibold text-slate-400">
                         <span className="text-gray-600">Drop</span>Cart<span className="text-gray-600 text-5xl leading-0">.</span>
-                        <p className="absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-green-500">
+                        <p className="absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-gray-500">
                             .NG
                         </p>
                         <Show when={{ plan: 'plus' }}>   
